4771eea8d54e4f7ed60ff567a5f737b13921a70f
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-05-28 20:55:58 -0500

    Project Finalized, ready for review

---
+++ src/components/Course.js
@@ -0,0 +1,98 @@
+import { hasConflict, timeParts } from '../utilities/times';
+import React, { useState, useEffect } from 'react';
+import { setData, signInWithGoogle, signOut, useUserState } from '../utilities/firebase';
+
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+
+const getCourseMeetingData = course => {
+  const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+  const valid = !meets || timeParts(meets).days;
+  if (valid) return meets;
+  alert('Invalid meeting data');
+  return null;
+};
+
+const reschedule = async (course, meets) => {
+  if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+    try {
+      await setData(`/courses/${course.id}/meets`, meets);
+    } catch (error) {
+      alert(error);
+    }
+  }
+};
+
+export const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
+  const [user] = useUserState();
+  const style = {
+    backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
+  };
+
+  return (
+    <div className="card m-1 p-2" 
+        style={style}
+        onClick={(isDisabled) ? null : () => setSelected(toggle(course, selected))}
+        onDoubleClick={!user ? null : () => reschedule(course, getCourseMeetingData(course))}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+        <div className="card-text">{ course.title }</div>
+        <div className="card-text">{ course.meets }</div>
+      </div>
+    </div>
+  );
+};
+
+export const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const SignInButton = () => (
+  <button className="btn btn-secondary btn-sm"
+      onClick={() => signInWithGoogle()}>
+    Sign In
+  </button>
+);
+
+const SignOutButton = () => (
+  <button className="btn btn-secondary btn-sm"
+      onClick={() => signOut()}>
+    Sign Out
+  </button>
+);
+
+export const TermSelector = ({term, setTerm}) => {
+  const [user] = useUserState();
+  return (
+    <div className="btn-toolbar justify-content-between">
+      <div className="btn-group">
+      { 
+        Object.values(terms).map(
+          value => <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+        )
+      }
+      </div>
+      { user ? <SignOutButton /> : <SignInButton /> }
+    </div>
+  );
+};
+
+export const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+export const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
---
+++ src/utilities/firebase.js
@@ -0,0 +1,73 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import { getAuth, GoogleAuthProvider, onIdTokenChanged, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState, useEffect } from 'react';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBd4KdoVh9NP_XX7rDi3jhIbHCnSzrDwQk",
+  authDomain: "scheduler-5cecc.firebaseapp.com",
+  databaseURL: "https://scheduler-5cecc-default-rtdb.firebaseio.com",
+  projectId: "scheduler-5cecc",
+  storageBucket: "scheduler-5cecc.appspot.com",
+  messagingSenderId: "808765753703",
+  appId: "1:808765753703:web:55d6fdb1a6c3d3651087c2",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+
+export const useData = (path, transform) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const dbRef = ref(database, path);
+    const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+    if (devMode) { console.log(`loading ${path}`); }
+    return onValue(dbRef, (snapshot) => {
+      const val = snapshot.val();
+      if (devMode) { console.log(val); }
+      setData(transform ? transform(val) : val);
+      setLoading(false);
+      setError(null);
+    }, (error) => {
+      setData(null);
+      setLoading(false);
+      setError(error);
+    });
+  }, [path, transform]);
+
+  return [data, loading, error];
+};
+
+export const setData = (path, value) => (
+  set(ref(database, path), value)
+);
+
+export const signInWithGoogle = () => {
+  signInWithPopup(getAuth(firebase), new GoogleAuthProvider());
+};
+
+const firebaseSignOut = () => signOut(getAuth(firebase));
+
+export const useUserState = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onIdTokenChanged(getAuth(firebase), setUser);
+  }, []);
+
+  return [user];
+};
+
+
+export { firebaseSignOut as signOut };
