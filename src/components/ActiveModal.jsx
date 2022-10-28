77098dc2ff1d47340595ce5718d514c52cbe42c6
Dimitrios Mavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-10-28 15:35:48 -0500

    Project Finalized!

--- src/components/ActiveModal.jsx
+++ src/components/ActiveModal.jsx
@@ -4,10 +4,9 @@ const ActiveModal = ({ selected }) => (
     selected.length === 0 ?
         (<div>
           <h2>Oops.. It seems that you have not selected a class yet.</h2>
-          <p>Please click on any class to select it and double click on it to edit it's details. In order for the edit feature to work you need to be signed in.</p>
+          <p>Please click on any class to select it and click edit on it to edit it's details. In order for the edit feature to appear and work you need to be signed in.</p>
         </div>)
       : (<div>
-            <p>Please click on any class to select it and double click on it to edit it's details. In order for the edit feature to work you need to be signed in.</p>
             <h2>You selected:</h2>
             { Object.entries(selected).map(([id, course]) => (
                   <div key={id}>
