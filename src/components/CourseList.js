7815e0c1ae3886f6179de9c434f91d21506850f6
Dimitrios Mavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-11-07 16:44:14 -0600

    Test 3: Test interaction Completed
    Test 3: Test interaction Completed

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -35,7 +35,7 @@ export const TermButton = ({term, setTerm, checked}) => (
   <>
     <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
       onChange={() => setTerm(term)} />
-    <label className="btn btn-success m-1 p-2" htmlFor={term}>
+    <label className="btn btn-success m-1 p-2" htmlFor={term} data-cy={term}>
     { term }
     </label>
   </>
